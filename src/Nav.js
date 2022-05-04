0ae7e5ae71b47eabe25c436ab14992b466f9988e
ClaytonDewey <clay@dryadmedia.com>
2022-05-04 08:14:25 -0500

    Used useLocation hook from react router dom

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+
+import { Link, useLocation } from "react-router-dom";
 import { Menu } from "antd";
 import { HomeOutlined, UserOutlined, ProfileOutlined } from "@ant-design/icons";
+
 import { Hub } from "aws-amplify";
 import { checkUser } from "./checkUser";
 
-export const Nav = ({ current }) => {
+export const Nav = () => {
+    const loc = useLocation();
+    console.log(loc);
+    const splitLoc = loc.pathname.split("/");
+    console.log(splitLoc);
+    const key = splitLoc[1] && splitLoc[1].length > 0 ? splitLoc[1] : "home";
+    console.log(key);
+
     const [user, updateUser] = useState({});
 
     useEffect(() => {
@@ -26,7 +35,7 @@ export const Nav = ({ current }) => {
 
     return (
         <div>
-            <Menu selectedKeys={[current]} mode="horizontal">
+            <Menu selectedKeys={[key]} mode="horizontal">
                 <Menu.Item key="home">
                     <Link to={`/`}>
                         <HomeOutlined />
